beefc63239f6b8e35edca59d6f4d9fbb300cc563
KiriHans <github.mxuxw@slmail.me>
2024-03-19 22:24:44 -0500

    pass 4nd stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -3,10 +3,31 @@
 #include <string>
 #include <cstring>
 #include <unistd.h>
+#include <fcntl.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/epoll.h>
+
 #include <arpa/inet.h>
 #include <netdb.h>
+
+const int MAX_EVENTS = 10;
+const int BUFF_SIZE = 1024;
+
+void handle_client(int client_fd)
+{
+  char buffer[BUFF_SIZE];
+  ssize_t read_client_bytes = read(client_fd, buffer, BUFF_SIZE - 1);
+  if (read_client_bytes <= 0)
+  {
+    std::cerr << "An error has ocurred" << std::endl;
+  }
+
+  std::string write_string = "+PONG\r\n";
+  int size_string = strlen(write_string.c_str());
+  auto sent_fd = write(client_fd, write_string.c_str(), size_string);
+}
 
 int main(int argc, char **argv)
 {
@@ -54,30 +75,70 @@ int main(int argc, char **argv)
 
   std::cout << "Waiting for a client to connect...\n";
 
-
-
-  int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
-  int sent_fd = 0;
-  char buffer[1024];
-
-  if (client_fd != 0)
+  int flags = fcntl(server_fd, F_GETFL, 0);
+  if (flags == -1)
   {
-    std::cout << "Client connected\n";
+    std::cerr << "Error getting flags" << std::endl;
+    close(server_fd);
+    return 1;
+  }
 
-    std::string write_string = "+PONG\r\n";
-    
-    while (read(client_fd, buffer, sizeof(buffer) - 1) > 0)
+  if (fcntl(server_fd, F_SETFL, flags |= O_NONBLOCK) == -1)
+  {
+    std::cerr << "Error setting sockets to Non-blocking" << std::endl;
+    close(server_fd);
+    return 1;
+  }
+
+  struct epoll_event ev, events[MAX_EVENTS];
+
+  int epoll_fd = epoll_create1(0);
+  if (epoll_fd == -1)
+  {
+    std::cerr << "epoll_create1 failed" << std::endl;
+    return 1;
+  }
+
+  ev.events = EPOLLIN;
+  ev.data.fd = server_fd;
+  if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &ev) == -1)
+  {
+    std::cerr << "epoll_create1 failed" << std::endl;
+    close(epoll_fd);
+    return 1;
+  }
+
+  while (true)
+  {
+    int num_fd = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+    for (size_t n = 0; n < num_fd; n++)
     {
-      int size_string = strlen(write_string.c_str());
-      sent_fd = write(client_fd, write_string.c_str(), size_string);
-      
-      if(sent_fd == -1){
-        break;
+      if (events[n].data.fd == server_fd)
+      {
+        int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
+        if (client_fd == -1)
+        {
+          std::cerr << "Accept() failed" << std::endl;
+          continue;
+        }
+
+        ev.events = EPOLLIN | EPOLLET;
+        ev.data.fd = client_fd;
+
+        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &ev) == -1)
+        {
+          std::cerr << "Failed to add client file descriptor to epoll" << std::endl;
+          close(client_fd);
+          continue;
+        }
+      } else {
+        handle_client(events[n].data.fd);
       }
-    };
+    }
   }
 
   close(server_fd);
+  close(epoll_fd);
 
   return 0;
 }
